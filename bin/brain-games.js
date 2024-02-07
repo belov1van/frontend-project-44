abe8b5a7fe904ba652f0ccbe34b4cb5e41948684
Mac <mac@MacBook-Mac.local>
2024-02-07 14:09:50 +0300

    even games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,12 @@
+
 import { getUserName } from "../src/cli.js";
 
 getUserName;
+
+import { playEvenGame } from "./brain-even.js";
+
+playEvenGame;
+
+import { getRandomNumber } from "./brain-even.js";
+
+getRandomNumber;
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
-import readlineSync from 'readline-sync';
-
+import readlineSync from 'readline-sync'
+console.log('Welcome to the brain games');
 export function getUserName() {
   // Use the `question` function to prompt the user for input
   const name = readlineSync.question("What is your name? ")
